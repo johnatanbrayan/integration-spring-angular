659045ee95e4f653cf73de04967c74f3d981a4e0
Johnatan Brayan <johnatanbrayan1@gmail.com>
2021-03-18 11:26:51 -0300

    Inicializing project

---
+++ src/main/java/br/com/johnatanbrayan/integrationspringangular/IntegrationSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.johnatanbrayan.integrationspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationSpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/johnatanbrayan/integrationspringangular/IntegrationSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johnatanbrayan.integrationspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
